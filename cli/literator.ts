a788dfcc3d2ca6fceaa24e9e94b956b645a7aecf
mistlog <mistlog@outlook.com>
2020-02-15 13:16:04 +0800

    export utility function for plugin use

--- cli/literator.ts
+++ cli/literator.ts
@@ -1,10 +1,10 @@
-import { outputFileSync, removeSync, readFileSync, existsSync } from "fs-extra";
+import { outputFileSync, removeSync, readFileSync, existsSync, readFile, pathExists } from "fs-extra";
 import * as traverse from "filewalker";
 import * as watch from "node-watch";
 import { SvelteTranscriber } from "../src";
 import { config } from "./config";
 import { Transcriber } from "typedraft";
-import { transformSync } from "@babel/core";
+import { transformSync, transformAsync } from "@babel/core";
 import * as TypescriptPreset from "@babel/preset-typescript";
 
 function TraverseDirectory(path: string, callback: (name: string, path: string) => void)
@@ -91,15 +91,34 @@ export function ComposeFile(source: string)
 {
     if (source.endsWith(".svelte.tsx"))
     {
-        TranscribeSvelteDraft(source);
+        const component = TranscribeSvelteDraftSync(source);
+        outputFileSync(source.replace(".tsx", ""), component, "utf8");
     }
     else if (source.endsWith(".js.tsx"))
     {
-        TranscribeTypeDraft(source);
+        const code = TranscribeTypeDraftSync(source);
+        outputFileSync(source.replace(".tsx", ""), code, "utf8");
     }
 }
 
-function TranscribeTypeDraft(source: string)
+export async function TranscribeTypeDraftAsync(source: string)
+{
+    const code = await readFile(source, "utf8");
+    const transcriber = new Transcriber(code);
+    config.dsls.forEach(dsl => transcriber.AddDSL(dsl.name, dsl.dsl));
+
+    const ts_code = transcriber.Transcribe();
+
+    const js_code = await transformAsync(ts_code, {
+        filename: "script.tsx",
+        ast: true,
+        presets: [[TypescriptPreset, { jsxPragma: "preserve", isTSX: true, allExtensions: true }]]
+    });
+
+    return js_code.code;
+}
+
+export function TranscribeTypeDraftSync(source: string)
 {
     const code = readFileSync(source, "utf8");
     const transcriber = new Transcriber(code);
@@ -113,10 +132,39 @@ function TranscribeTypeDraft(source: string)
         presets: [[TypescriptPreset, { jsxPragma: "preserve", isTSX: true, allExtensions: true }]]
     }).code;
 
-    outputFileSync(source.replace(".tsx", ""), js_code, "utf8");
+    return js_code;
 }
 
-function TranscribeSvelteDraft(source: string)
+export async function TranscribeSvelteDraftAsync(source: string)
+{
+    const code = await readFile(source, "utf8");
+    const transcriber = new SvelteTranscriber(code);
+    config.dsls.forEach(dsl => transcriber.AddDSL(dsl.name, dsl.dsl));
+    const { import_section, script_section, template_section } = transcriber.TranscribeToSections();
+
+    //
+    const style = source.replace(".svelte.tsx", ".css");
+    const style_section = await pathExists(style) ? await readFile(style, "utf8") : "";
+
+    //
+    const component = [
+        "<script>",
+        import_section,
+        "\n",
+        script_section,
+        "</script>",
+        "\n",
+        template_section,
+        "\n",
+        "<style>",
+        style_section,
+        "</style>"
+    ].join("\n");
+
+    return component;
+}
+
+export function TranscribeSvelteDraftSync(source: string)
 {
     const code = readFileSync(source, "utf8");
     const transcriber = new SvelteTranscriber(code);
@@ -141,5 +189,6 @@ function TranscribeSvelteDraft(source: string)
         style_section,
         "</style>"
     ].join("\n");
-    outputFileSync(source.replace(".tsx", ""), component, "utf8");
+
+    return component;
 }
