eaac094847353efe66ab88dc648824422e826f60
mistlog <mistlog@outlook.com>
2020-02-26 21:51:44 +0800

    fix bug: special elements closing tag not translated

--- test/section/template-section.test.ts
+++ test/section/template-section.test.ts
@@ -397,7 +397,11 @@ describe("translate template section", () =>
             export default function App()
             {
                 let file: string;
-                <svelte-self {...file}/>
+                <svelte-self {...file}/>;
+
+                <svelte-head>
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+                </svelte-head>
             }
         `;
 
